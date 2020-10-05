ed901f16907dd4adfe0654ba93c0aa8c5666ae1f
dkippes <kippes.diego@gmail.com>
2020-10-05 16:56:01 -0300

    formulario perfil

--- routes/mercado_liebre.js
+++ routes/mercado_liebre.js
@@ -2,42 +2,42 @@ var express = require('express');
 var router = express.Router();
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
+router.get('/', function (req, res, next) {
   res.render('mercado_liebre');
 });
 
 /* GET Ofertas page. */
-router.get('/ofertas', function(req, res, next) {
+router.get('/ofertas', function (req, res, next) {
   res.render('ofertas');
 });
 
 /* GET Tiendas Oficiales page. */
-router.get('/tiendas', function(req, res, next) {
+router.get('/tiendas', function (req, res, next) {
   res.render('tiendas');
 });
 
 /* GET Vender page. */
-router.get('/vender', function(req, res, next) {
+router.get('/vender', function (req, res, next) {
   res.render('vender');
 });
 
 /* GET Ayuda page. */
-router.get('/ayuda', function(req, res, next) {
+router.get('/ayuda', function (req, res, next) {
   res.render('ayuda');
 });
 
 /* GET Crea tu cuenta page. */
-router.get('/crea_cuenta', function(req, res, next) {
-  res.render('crea_cuenta');
+router.get('/register', function (req, res, next) {
+  res.render('register');
 });
 
 /* GET Ingresa page. */
-router.get('/ingresa', function(req, res, next) {
-  res.render('ingresa');
+router.get('/login', function (req, res, next) {
+  res.render('login');
 });
 
 /* GET Mis Compras page. */
-router.get('/mis_compras', function(req, res, next) {
+router.get('/mis_compras', function (req, res, next) {
   res.render('mis_compras');
 });
 
